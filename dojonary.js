413d8f8f778882c657e0060e1127d37e65eaed17
Greg <gharris9891@yahoo.com>
2023-06-08 23:02:32 -0400

    first commit

---
+++ dojonary.js
@@ -0,0 +1,13 @@
+function logout(element) {
+    console.log('abc')
+    element.innerText="Logout";
+}
+
+function message() {
+    console.log("connected")
+    alert("Hello");
+}
+
+function hide(element) {
+    element.remove();
+}
